c340ddcdf14453d7f996b8d6420b58560147f4ee
Nick Cash <nickcash@Nicks-MacBook-Pro.local>
2017-04-22 15:36:06 -0500

    Initial settings for pixel perfect rendering

---
+++ Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+
+    private void FixedUpdate()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+
+        if (!Mathf.Approximately(moveHorizontal, 0.0f))
+        {
+            var movement = new Vector3(moveHorizontal, 0.0f);
+            rb.velocity = movement * speed;
+        }
+    }
+}
