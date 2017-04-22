6d76d69765c5fe14aad1dc8a61b0589daa369548
Nick Cash <nickcash@Nicks-MacBook-Pro.local>
2017-04-22 13:28:57 -0500

    Basic scene wiring

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string destinationSceneName;
+
+    public void Go()
+    {
+        SceneManager.LoadScene(destinationSceneName);
+    }
+}
