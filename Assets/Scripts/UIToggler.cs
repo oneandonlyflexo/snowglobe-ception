3ae278f13486db25e076bc946cdec3ac916531ce
Nick Cash <nickcash@Nicks-MacBook-Pro.local>
2017-04-23 03:32:03 -0500

    UI and animals

---
+++ Assets/Scripts/UIToggler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class UIToggler : MonoBehaviour
+{
+    public UnityEvent triggerEnter;
+    public UnityEvent triggerExit;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "UIEnabler")
+        {
+            triggerEnter.Invoke();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "UIEnabler")
+        {
+            triggerExit.Invoke();
+        }
+    }
+}
