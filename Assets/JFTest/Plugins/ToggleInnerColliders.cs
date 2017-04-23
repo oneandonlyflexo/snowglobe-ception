a06584c30edb29e75646d9e4f042b2b3e1eb1c20
JeremyFelege <jeremyfelege@gmail.com>
2017-04-23 03:03:08 -0500

    hooked up the listeners to handle toggling the overlays

---
+++ Assets/JFTest/Plugins/ToggleInnerColliders.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleInnerColliders : MonoBehaviour {
+
+    bool isInside = false;
+
+    public List<GameObject> children = new List<GameObject>();
+
+    private void Awake()
+    {
+        EventManager.Listen("ToggleIndoors", ToggleIndoorOutDoor);
+    }
+
+    private void OnDisable()
+    {
+        EventManager.StopListen("ToggleIndoors", ToggleIndoorOutDoor);
+    }
+
+    private void ToggleIndoorOutDoor()
+    {
+        isInside = !isInside;
+
+        foreach (var record in children)
+        {
+           record.SetActive(isInside);
+        }
+    }
+}
